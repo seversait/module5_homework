0b1d7b18226b8374f051b934406501889848ab40
seversait <120575725+seversait@users.noreply.github.com>
2023-01-31 17:46:38 +0300

    Update 5.1.js

--- 5.1.js
+++ 5.1.js
@@ -1,1 +1,21 @@
 
+
+let a = prompt ("Введите значение");
+
+if ((typeof(+a) == 'number') && (!isNaN(+a))){
+
+    if (a % 2 === 0) {
+
+        console.log("Четное");
+
+    } else {
+
+        console.log("Нечетное");
+
+    }
+
+} else {
+
+    console.log("Упс, кажется, вы ошиблись");
+
+}
