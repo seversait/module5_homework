0a933132ff9275be9952e794f28df7515b83e0e3
seversait <120575725+seversait@users.noreply.github.com>
2023-01-31 17:54:42 +0300

    Create 5.6.js

---
+++ 5.6.js
@@ -0,0 +1,27 @@
+let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 's'];
+
+let theSame = true;
+
+for (let i = 0; i < arr.length - 1; i++){
+
+    for (let j = i + 1; j < arr.length; j++){
+
+        if (typeof(arr[i]) !== typeof(arr[j])){
+
+            theSame = false;
+
+        }
+
+    }
+
+}
+
+if (theSame) {
+
+    console.log("array is the same");
+
+} else {
+
+    console.log("array is not the same");
+
+}
